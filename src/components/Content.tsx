76f6e7a04531ffe7d6581b5da3e92caf0c702ea9
Heron Eto <41599309+heroneto@users.noreply.github.com>
2021-09-17 20:35:22 -0300

    feat: added Content and Sidebar components

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,29 @@
-export function Content() {
-  // Complete aqui
+import { MovieCard } from './MovieCard';
+
+interface MovieProps {
+  imdbID: string;
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+  Runtime: string;
+}
+
+interface IContentProps {
+  movies: MovieProps[]
+}
+
+
+export function Content({movies}:IContentProps) {
+  return (
+    <main>
+    <div className="movies-list">
+      {movies.map(movie => (
+        <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+      ))}
+    </div>
+  </main>
+  )
 }
